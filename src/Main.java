4d20efb7e291a89c1a6d6f5b03b9f7e3864dda3d
0xRayan14 <rayan.seghrouchni@jobtrek.ch>
2023-10-20 12:59:26 +0200

    Created winning combinations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static String[] board;
+    static String turn;
+
+    static String checkWinner() {
+        String[] winningCombinations = {"123", "147", "159", "258", "357",  "369", "456", "789"};
+
+        for (String line : winningCombinations) {
+            String sequence = "" + board[line.charAt(0) - '1'] + board[line.charAt(1) - '1'] + board[line.charAt(2) - '1'];
+
+            if (sequence.equals("XXX")) return "X";
+            if (sequence.equals("OOO")) return "O";
+        }
+
+        if (Arrays.stream(board).allMatch(s -> s.equals("X") || s.equals("O"))) return "draw";
+
+        return null;
     }
 }
